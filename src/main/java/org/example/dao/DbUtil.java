f94e3332a4f7dc9bddbce58305bcfbc471696ce8
Martyna <martyna.matuszak96@gmail.com>
2021-02-08 15:49:41 +0100

    changed db name

--- src/main/java/org/example/dao/DbUtil.java
+++ src/main/java/org/example/dao/DbUtil.java
@@ -19,7 +19,7 @@ public class DbUtil {
             try {
                 Context initContext = new InitialContext();
                 Context envContext = (Context)initContext.lookup("java:/comp/env");
-                dataSource = (DataSource)envContext.lookup("jdbc/school");
+                dataSource = (DataSource)envContext.lookup("jdbc/workshops_2");
             } catch (NamingException e) { e.printStackTrace(); }
         }
         return dataSource;
