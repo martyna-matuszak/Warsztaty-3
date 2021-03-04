87ae36373a536bb8ee96471f6f337c95638aee5a
Martyna <martyna.matuszak96@gmail.com>
2021-03-04 10:10:53 +0100

    equals and hashcode implemented in User

--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -66,4 +66,32 @@ public class User {
     public String toString() {
         return "ID: " + this.id + ", username: " + this.username + ", email: " + this.email + ", group id: " + this.userGroupId;
     }
+
+    @Override public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        User user = (User) o;
+
+        if (id != user.id)
+            return false;
+        if (userGroupId != user.userGroupId)
+            return false;
+        if (username != null ? !username.equals(user.username) : user.username != null)
+            return false;
+        if (email != null ? !email.equals(user.email) : user.email != null)
+            return false;
+        return password != null ? password.equals(user.password) : user.password == null;
+    }
+
+    @Override public int hashCode() {
+        int result = id;
+        result = 31 * result + (username != null ? username.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + userGroupId;
+        return result;
+    }
 }
